f20be84daa6692c983835a996340751e80ac97fb
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-05-06 01:50:59 +0600

    add rows

--- back/database/migrations/2023_05_04_075207_create_contractors_table.php
+++ back/database/migrations/2023_05_04_075207_create_contractors_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('type');
+            $table->string('country');
+            $table->string('INN');
             $table->timestamps();
         });
     }
